13d46e4fc3a2d970d27e3c57b27cc791420ad0ae
oln <victorprg0111@gmail.com>
2025-12-10 13:27:33 -0300

    fix: waybar script check for wrong zone

--- dotfiles/.config/waybar/scripts/temp.sh
+++ dotfiles/.config/waybar/scripts/temp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Read zone 1 if it exists, otherwise zone 0
-if [ -f /sys/class/thermal/thermal_zone1/temp ]; then
+if [ -f /sys/class/thermal/thermal_zone7/temp ]; then
   cat /sys/class/thermal/thermal_zone7/temp | awk '{print int($1/1000)}'
 else
   cat /sys/class/thermal/thermal_zone0/temp | awk '{print int($1/1000)}'
