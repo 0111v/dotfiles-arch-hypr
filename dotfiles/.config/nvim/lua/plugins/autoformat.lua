df92dbb11ade63c7cc2c12d238be5e6e7f07fea8
oln <victorprg0111@gmail.com>
2025-12-14 12:33:43 -0300

    add: new formatters

--- dotfiles/.config/nvim/lua/plugins/autoformat.lua
+++ dotfiles/.config/nvim/lua/plugins/autoformat.lua
@@ -35,7 +35,18 @@ return {
       -- python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd', 'prettier', stop_after_first = true },
+      jsonc = { 'prettierd', 'prettier', stop_after_first = true },
+      yaml = { 'prettierd', 'prettier', stop_after_first = true },
+
+      -- Web
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      scss = { 'prettierd', 'prettier', stop_after_first = true },
+
+      -- Markdown
+      markdown = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
